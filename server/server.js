9579cea8b5d63db413d10074f14c8165ac5d73f5
Tharusha-2000 <112651385+Tharusha-2000@users.noreply.github.com>
2024-01-31 00:52:53 +0530

    connet db

--- server/server.js
+++ server/server.js
@@ -1,39 +1,21 @@
-import express from "express";
-import cors from 'cors'
-import { adminRouter } from "./Routes/AdminRoute.js";
-import { EmployeeRouter } from "./Routes/EmployeeRoute.js";
-import Jwt from "jsonwebtoken";
-import cookieParser from "cookie-parser";
+const express = require("express");
+const path = require("path");
 
-const app = express() 
-app.use(cors({
-    origin: ["http://localhost:5173"],
-    methods: ['GET', 'POST', 'PUT', "DELETE"],
-    credentials: true
-}))
-app.use(express.json())
-app.use(cookieParser())
-app.use('/auth', adminRouter)
-app.use('/employee', EmployeeRouter)
-app.use(express.static('Public'))
+const app = express();
 
-const verifyUser = (req, res, next) => {
-    const token = req.cookies.token;
-    if(token) {
-        Jwt.verify(token, "jwt_secret_key", (err ,decoded) => {
-            if(err) return res.json({Status: false, Error: "Wrong Token"})
-            req.id = decoded.id;
-            req.role = decoded.role;
-            next()
-        })
-    } else {
-        return res.json({Status: false, Error: "Not autheticated"})
-    }
+const dbconfig = require("./utils/db.js");
+const userRoute = require("./routes/usersRoute.js");
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use(express.json());
+
+app.use("/api/users", userRoute);
+
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
 }
-app.get('/verify',verifyUser, (req, res)=> {
-    return res.json({Status: true, role: req.role, id: req.id})
-} )
 
-app.listen(3000, () => {
-    console.log("Server is running")
-})
+app.listen(port, () => console.log("Node Server Started using Nodemon!"));
