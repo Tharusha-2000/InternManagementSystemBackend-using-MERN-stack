809a1dd770fbceae26c7718f623a35b4544828da
Tharusha-2000 <112651385+Tharusha-2000@users.noreply.github.com>
2024-02-02 00:24:09 +0530

    add register part backend

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,7 +3,34 @@ const cors = require('cors')
 const bcrypt = require('bcryptjs')
 const jwt=require('jsonwebtoken')
 const cookieParser=require('cookie-parser')
+
+
 const userModel = require('./models/user')
+
+
+
+
+router.post('/add_user', (req, res) => {
+  // Hash the password before saving
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) return res.json({ status: false, error: "Hashing Error" });
+
+      // Create a new user document
+       const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: hash,
+           
+      });
+
+      // Save the user document to the database
+      newUser.save((err, result) => {
+          if (err) return res.json({ status: false, error: err });
+          return res.json({ status: true });
+      });
+  });
+});
+
 
 const app = express()
 app.use(express.json())
