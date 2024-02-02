a4e07ed453c8a4f3180b84e1769f64b3cfb18a6d
Tharusha-2000 <112651385+Tharusha-2000@users.noreply.github.com>
2024-02-02 12:33:02 +0530

    delete js files

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-
-
-
-
-
 router.post("/register", async (req, res) => {
   const { email, password } = req.body;
 
