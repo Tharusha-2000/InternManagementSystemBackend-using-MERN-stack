fb579892ad2c8891d49d24bc16464f641fd51e0d
Tharusha-2000 <112651385+Tharusha-2000@users.noreply.github.com>
2024-03-30 01:32:26 +0530

    create file structure

--- server/config.js
+++ server/config.js
@@ -1,3 +1,6 @@
 module.exports={
-    JWT_SECRET:"lQNsjUq90WYzhvsJd7a+9x0nWlZNBkfTap2sv8Ts158="
+    JWT_SECRET:"lQNsjUq90WYzhvsJd7a+9x0nWlZNBkfTap2sv8Ts158=",
+    Email:'IMSystem99x@gmail.com',
+    Password:'jqlwlkuvbtrmofmj'
+
 };
