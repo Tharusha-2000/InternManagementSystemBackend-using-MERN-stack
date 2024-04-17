b6d2990ee025654734ebd1645debed7e529ebcae
tharushadinuth <112651385+Tharusha-2000@users.noreply.github.com>
2024-04-18 03:18:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const app = express()
 
 app.use(cors({
     origin: ["https://frontend-indol-three-11.vercel.app"],
-    methods: ['GET', 'POST', 'PUT', "DELETE"],
+    methods: ['GET','POST','PUT',"DELETE"],
     credentials: true
 }))
 
