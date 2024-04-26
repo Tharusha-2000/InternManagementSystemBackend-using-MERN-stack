758802ac22cb3b8f63474e975545f7fce9dd8b84
tharushadinuth <112651385+Tharusha-2000@users.noreply.github.com>
2024-04-26 18:18:43 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const body=require('body-parser');
 const app = express() 
 
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://imsfrontend.vercel.app"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
  }))
