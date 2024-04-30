d802a8d66106f79df9be3b9665370835a5e17b86
tharushadinuth <112651385+Tharusha-2000@users.noreply.github.com>
2024-04-30 18:23:10 +0530

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,13 +1,19 @@
 const  mongoose  = require("mongoose");
 var mongoURL = process.env.DBUrl;
-mongoose.connect(process.env.MONGODB_URI || mongoURL)
-    .then(() => {
-
-        console.log('MongoDB Connection Successful');
-      })
-      .catch((error) => {
-        console.error('MongoDB Connection Failed:', error);
-      });
-  
-
-module.exports = mongoose;
+const connectDB = async() => {
+        try {
+           mongoose.set("strictQuery", false);
+            await mongoose.connect(mongoURL,{
+               useUnifiedTopology: true,
+               useNewUrlParser: true,
+            })
+            console.log('mongodb connection SUCCESS');
+        } catch (error) {
+            console.log('mongodb connection FAIL');
+            console.log(error);
+            process.exit(1);
+        }
+   
+   }
+   
+   module.exports = connectDB;
