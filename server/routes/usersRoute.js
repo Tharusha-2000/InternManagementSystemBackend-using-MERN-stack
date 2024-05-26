4f19e3920ab843768cf8317ee8ed6276d274b13e
DilumCA <andradiwmdc.21@uom.lk>
2024-05-26 17:57:17 +0530

    my apis

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -85,11 +85,49 @@ module.exports = router;
 
 /*......................................hansi.......................*/
 
+
 /*......................................dilum.......................*/
 
+//router.get("/interns",controller.getInterns);
+const {getEvInterns} = require('../authcontrol/controller');
+router.get('/Evinterns', getEvInterns);
+
+
+//router to get evaluators
+router.get('/evaluators', middleware.Auth, controller.getEvaluators);
+//rout to post evaluator name into evaluationformdetails collection
+const {postEvaluatorName} = require('../authcontrol/controller');
+router.post('/evaluatorname', postEvaluatorName);
+
+//router to delete evaluationform details
+const {deleteeformData} = require('../authcontrol/controller');
+router.delete('/deleteeformData', deleteeformData);
 
 
 
+
+
+//mentor pages routes
+const{checkMentor} = require('../authcontrol/controller');
+router.get('/checkMentor/:userId', checkMentor);
+
+//get critirias for mentor
+const {getCriteriaById} = require('../authcontrol/controller');
+router.get('/getCriteriaById/:id', getCriteriaById);
+
+
+//tempory routing for adding remaining feilds in collection
+const {setDefaultEformstates} = require('../authcontrol/controller');
+router.post('/setDefaultEformstates', setDefaultEformstates);
+
+//routes for store mentor scores of evaluation forms
+const { storeMentorScoresById } = require('../authcontrol/controller');
+router.post('/storeMentorScores/:id', storeMentorScoresById);
+
+//routes for evaluators section
+//get all the interns by evaluator
+const{getInternsByEvaluator} = require('../authcontrol/controller');
+router.get('/getInternsByEvaluator/:id',getInternsByEvaluator);
 /*......................................dilum.......................*/
 
 
